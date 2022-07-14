0765cba6070bb4134fd8180e3f62a20aa22f702f
harshrocks <ms744311@gmail.com>
2022-07-14 16:20:02 +0530

    port changed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = process.env.PORT || 12334;
+const PORT = process.env.PORT || 3000;
 const BookRoute = require('./quotes_routes');
 const swaggerJs = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
