21466e8b2cfce17bede437329bdd0ba9a708eb75
harshrocks <ms744311@gmail.com>
2022-07-14 15:13:59 +0530

    changed the url and the port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 12334;
+const PORT = process.env.PORT || 3000;
 const BookRoute = require('./quotes_routes');
 
 require("dotenv").config();
@@ -46,7 +46,6 @@ app.get('/quotes/genre/:genre', (req, res) => {
         console.log(err);
       }
       res.status(200).json(result);
-      // console.log(result);
     }
   )
 });
@@ -70,7 +69,6 @@ app.get('/quotes', (req, res) => {
         console.log(err);
       }
       res.status(200).json(result);
-      // console.log(result);
     }
   )
 });
